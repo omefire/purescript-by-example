468353c12d727a7cca7dc9bffc508812b643fae3
Omar Mefire <omefire@gmail.com>
2017-11-12 11:34:11 -0800

    Chapter 3

---
+++ ch3/src/Data/AddressBook.purs
@@ -0,0 +1,49 @@
+module Data.AddressBook where
+
+import Prelude
+
+import Control.Plus (empty)
+import Data.List (List(..), filter, head)
+import Data.Maybe (Maybe)
+import Control.Monad.Eff.Console (log)
+
+main = log "Hello, World!"
+
+type Entry =
+  {
+    firstName :: String
+   ,lastName  :: String
+   ,address   :: Address
+  }
+
+type Address =
+  {
+    street :: String
+   ,city   :: String
+   ,state  :: String
+  }
+
+type AddressBook = List Entry
+
+showEntry :: Entry -> String
+showEntry entry = entry.lastName <> ", " <>
+                  entry.firstName <> ": " <>
+                  showAddress entry.address
+
+showAddress :: Address -> String
+showAddress addr = addr.street <> ", " <>
+                   addr.city <> ", " <>
+                   addr.state
+
+emptyBook :: AddressBook
+emptyBook = empty
+
+insertEntry :: Entry -> AddressBook -> AddressBook
+insertEntry = Cons
+
+findEntry :: String -> String -> AddressBook -> Maybe Entry
+findEntry firstName lastName book = head <<< filter filterEntry
+  --head $ filter filterEntry book
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry = entry.firstName == firstName && entry.lastName == entry.lastName
